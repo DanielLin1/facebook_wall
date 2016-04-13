4ee9661c674ba1f483b85a223861f45d3e99fcfd
daniellin1 <dlin270@lmc896.org>
2016-04-13 20:26:55 +0000

    Added space to image

--- starter_code/js/wall.js
+++ starter_code/js/wall.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 	
 	$('button').click(function() {
 		var message = $('input').val();
-		$('#wall').prepend('<div><img src="https://media2.giphy.com/media/97DVwH0GcYZVu/200_s.gif" height=128px length=128px>'+message+'</div>')
+		$('#wall').prepend('<div><img src="https://media2.giphy.com/media/97DVwH0GcYZVu/200_s.gif" height=128px length=128px>'+  message+'</div>')
 	});
 
 	
