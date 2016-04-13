30c31cc4f73acdffa5f35f9592e4ba5f0544fb1e
daniellin1 <dlin270@lmc896.org>
2016-04-13 20:24:15 +0000

    wall

--- starter_code/js/wall.js
+++ starter_code/js/wall.js
@@ -1,6 +1,11 @@
-$(document).ready(function()
-{
-	$('#message').focus();
+$(document).ready(function(){
+	
+	$('button').click(function() {
+		var message = $('input').val();
+		$('#wall').prepend('<div><img src="https://media2.giphy.com/media/97DVwH0GcYZVu/200_s.gif" height=128px length=128px>'+message+'</div>')
+	});
+
+	
 
 	// YOUR CODE GOES HERE
 });
