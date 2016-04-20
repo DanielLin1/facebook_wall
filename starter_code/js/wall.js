de3b05cca878fae930f5bd1b510f6f0676f8f08e
daniellin1 <dlin270@lmc896.org>
2016-04-20 20:14:27 +0000

    Added like,hovor,amount of post

--- starter_code/js/wall.js
+++ starter_code/js/wall.js
@@ -2,9 +2,15 @@ $(document).ready(function(){
 	
 	$('button').click(function() {
 		var message = $('input').val();
-		$('#wall').prepend('<div><img src="https://media2.giphy.com/media/97DVwH0GcYZVu/200_s.gif" height=128px length=128px>'+  message+'</div>')
+		$('#wall').prepend('<div class="group"><img src="https://media2.giphy.com/media/97DVwH0GcYZVu/200_s.gif" height="128px" length="128px">'+  message+'<button> like </button></div>')
+		
+		var number = $("#number").html();
+	    console.log(number);
+	    number++;
+	    $("#number").html(number);
 	});
 
+	
 	
 
 	// YOUR CODE GOES HERE
